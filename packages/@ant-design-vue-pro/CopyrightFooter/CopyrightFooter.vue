faf4e339e575cfd6c84b471eaf5e687384ca3a79
limichange <limichange@hotmail.com>
2018-08-31 15:06:16 +0800

    init

---
+++ packages/@ant-design-vue-pro/CopyrightFooter/CopyrightFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.CopyrightFooter">
+    CopyrightFooter
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CopyrightFooter',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style module>
+.CopyrightFooter {
+  font-size: 20px;
+}
+</style>
