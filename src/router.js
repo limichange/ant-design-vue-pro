8b591ea959515d4984b5213e86381cabca174406
limichange <limichange@hotmail.com>
2018-09-01 15:36:16 +0800

    refactor: async import views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import CopyrightFooter from './views/CopyrightFooter'
 
 Vue.use(Router)
 
@@ -8,6 +7,6 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'CopyrightFooter',
-    component: CopyrightFooter
+    component: () => import('./views/CopyrightFooter')
   }]
 })
