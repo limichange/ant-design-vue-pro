7d7f7323b2b1358aa783efc6c964d9665be77a35
limichange <limichange@hotmail.com>
2018-09-17 16:50:39 +0800

    Update

--- src/components/SiderMenu/props.js
+++ src/components/SiderMenu/props.js
@@ -3,5 +3,5 @@ import VueTypes from 'vue-types'
 export default {
   width: VueTypes.number.def(200),
   logoSrc: VueTypes.string.def(''),
-  title: VueTypes.string.def('')
+  title: VueTypes.string.isRequired.def('')
 }
